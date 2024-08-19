8b75e9ea7608fdccc9155a068357cfdd6f620d77
Daniil Telegin <daniil.collis@gmail.com>
2024-08-19 19:10:51 +0300

    Add requirements.txt

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     name = request.args.get('name')
     message = request.args.get('message')
     return f"Hello {name}! {message}!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
